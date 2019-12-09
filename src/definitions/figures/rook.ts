eba5bf04afcd79ba5652e32933749650cccbeb49
Noah <>
2019-12-09 20:18:00 +0100

    Laws of Rook

--- src/definitions/figures/rook.ts
+++ src/definitions/figures/rook.ts
@@ -1,9 +1,25 @@
 import { Figure } from "./figure";
 import { TileColor } from "../tile"
+import { tileDescriptionToRowAndColumn, rowAndColumnToIndex } from "../functions"
+import { Board } from "../board"
 
 export class Rook extends Figure {
   getCharacter() {
     return this.color === TileColor.White ? "♖" : "♜"
   }
+  isPermittedMove(from: string, to: string, board: Board): boolean {
+    let [fromRow, fromColumn] = tileDescriptionToRowAndColumn(from)
+    let [toRow, toColumn] = tileDescriptionToRowAndColumn(to)
+
+    if(fromRow !== toRow && fromColumn !== toColumn) {
+      return false
+    }
+
+    // TODO: Rochade
+    // TODO: Figur im Weg
+    
+    return true
+  }
+
 
 }
