eed1f0afa1f5816d38837d27ab10873b146e9f3b
Noah <>
2019-12-10 21:08:30 +0100

    Laws of King

--- src/definitions/figures/king.ts
+++ src/definitions/figures/king.ts
@@ -1,9 +1,27 @@
 import { Figure } from "./figure";
 import { TileColor } from "../tile"
-import { tileDescriptionToRowAndColumn } from "../functions"
+import { tileDescriptionToRowAndColumn, rowAndColumnToIndex } from "../functions"
+import { Board } from "../board"
 
 export class King extends Figure {
   getCharacter() {
     return this.color === TileColor.White ? "♔" : "♚"
+  }
+  isPermittedMove(from: string, to: string, board: Board): boolean {
+    let [fromRow, fromColumn] = tileDescriptionToRowAndColumn(from)
+    let [toRow, toColumn] = tileDescriptionToRowAndColumn(to)
+    let toIndex = rowAndColumnToIndex(toRow, toColumn)
+    let fromIndex = rowAndColumnToIndex(fromRow, fromColumn)
+    let indexDistance = Math.abs(fromIndex - toIndex)
+
+    let validDistances = [1,7,8,9]
+    for (let distance of validDistances) {
+      if(indexDistance === distance) {
+        return true
+      }
+    }
+    // TODO: Patt
+    // TODO: Rochade
+    return false
   }
 }
