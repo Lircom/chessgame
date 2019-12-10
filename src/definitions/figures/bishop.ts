5de56e21aba6a67c11f8df318994e1596fabb67b
Noah <>
2019-12-10 21:02:56 +0100

    Laws of Bishop

--- src/definitions/figures/bishop.ts
+++ src/definitions/figures/bishop.ts
@@ -1,9 +1,24 @@
 import { Figure } from "./figure";
 import { TileColor } from "../tile"
-import { tileDescriptionToRowAndColumn } from "../functions"
+import { tileDescriptionToRowAndColumn, rowAndColumnToIndex } from "../functions"
+import { Board } from "../board"
+
 
 export class Bishop extends Figure {
   getCharacter() {
     return this.color === TileColor.White ? "♗" : "♝"
+  }
+  isPermittedMove(from: string, to: string, board: Board): boolean {
+    let [fromRow, fromColumn] = tileDescriptionToRowAndColumn(from)
+    let [toRow, toColumn] = tileDescriptionToRowAndColumn(to)
+    let columnDistance = Math.abs(toColumn - fromColumn)
+    let rowDistance = Math.abs(toRow - fromRow)
+
+    if( columnDistance !== rowDistance ) {
+      return false
+    }
+
+    // TODO: Figur im Weg
+    return true
   }
 }
