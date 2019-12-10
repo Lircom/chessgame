2cac60a77265acf46c6a4d344040b81b3024fe86
Noah <>
2019-12-10 21:02:22 +0100

    Laws of Knight

--- src/definitions/figures/knight.ts
+++ src/definitions/figures/knight.ts
@@ -1,9 +1,26 @@
 import { Figure } from "./figure";
 import { TileColor } from "../tile"
-import { tileDescriptionToRowAndColumn } from "../functions"
+import { tileDescriptionToRowAndColumn, rowAndColumnToIndex } from "../functions"
+import { Board } from "../board"
 
 export class Knight extends Figure {
   getCharacter() {
     return this.color === TileColor.White ? "♘" : "♞"
+  }
+  isPermittedMove(from: string, to: string, board: Board): boolean {
+    let [fromRow, fromColumn] = tileDescriptionToRowAndColumn(from)
+    let [toRow, toColumn] = tileDescriptionToRowAndColumn(to)
+    let toIndex = rowAndColumnToIndex(toRow, toColumn)
+    let fromIndex = rowAndColumnToIndex(fromRow, fromColumn)
+    let indexDistance = Math.abs(fromIndex - toIndex)
+
+    let validDistances = [17,15,6,10]
+    for (let distance of validDistances) {
+      if(indexDistance === distance) {
+        return true
+      }
+    }
+
+    return false
   }
 }
