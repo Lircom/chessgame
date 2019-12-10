136045ab123372bd368db6f03af208a859c01138
Noah <>
2019-12-10 21:17:44 +0100

    Laws of Queen

--- src/definitions/figures/queen.ts
+++ src/definitions/figures/queen.ts
@@ -1,8 +1,34 @@
 import { Figure } from "./figure";
 import { TileColor } from "../tile"
+import { tileDescriptionToRowAndColumn, rowAndColumnToIndex } from "../functions"
+import { Board } from "../board"
 
 export class Queen extends Figure {
   getCharacter() {
     return this.color === TileColor.White ? "♕" : "♛"
+  }
+
+  isPermittedMove(from: string, to: string, board: Board): boolean {
+    let [fromRow, fromColumn] = tileDescriptionToRowAndColumn(from)
+    let [toRow, toColumn] = tileDescriptionToRowAndColumn(to)
+    let toIndex = rowAndColumnToIndex(toRow, toColumn)
+    let fromIndex = rowAndColumnToIndex(fromRow, fromColumn)
+    let indexDistance = Math.abs(fromIndex - toIndex)
+    let validDistances = [1,7,8,9]
+
+    for (let distance of validDistances) {
+      if(indexDistance === distance) {
+        return true
+      }
+    }
+
+    let columnDistance = Math.abs(toColumn - fromColumn)
+    let rowDistance = Math.abs(toRow - fromRow)
+    if( columnDistance === rowDistance ) {
+      return true
+    }
+
+    // TODO: seitliche Bewegungen
+    return false
   }
 }
